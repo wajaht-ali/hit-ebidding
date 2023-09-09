0fa2a4911ce21124533e48df90cce6962acef8d2
Wajahat-404 <109886908+Wajahat-404@users.noreply.github.com>
2023-09-09 11:00:48 +0500

    styling and folder setting done

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import Home from "./components/Home";
+
+import "./styles/Utilities.scss";
+
 
 function App() {
   return (
-    <div className="App">
-      <h1>HIT Ebidding System</h1>
+    <div className="App" style={{display: "flex", flexDirection: "column", alignItems: "center"}}>
+
+      <Header />
+      <Home />
+      <Footer />
     </div>
   );
 }
