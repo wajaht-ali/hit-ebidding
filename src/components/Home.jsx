d5e92344ee94cdb1cc6227576b131f9447037f38
Wajahat-404 <109886908+Wajahat-404@users.noreply.github.com>
2023-09-12 12:00:19 +0500

    react carousle fixing

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,11 +1,35 @@
 import React from 'react'
 import "../styles/Home.scss";
+import { Carousel } from "react-responsive-carousel";
+import img1 from "../assets/img2.jpg";
+import img2 from "../assets/panzer.jpg";
+import img3 from "../assets/armed-veh.jpg";
+import "react-responsive-carousel/lib/styles/carousel.min.css";
 
 
 const Home = () => {
   return (
     <div className='home'>
-      <h2>Ebidding System</h2>
+      <div className="carousel">
+        <Carousel 
+        infiniteLoop
+        autoPlay
+        showStatus = {false}
+        showThumbs = {false}
+        interval = {3000}
+
+          className='slider'>
+          <div>
+            <img src={img1} alt='carousel-img' />
+          </div>
+          <div>
+            <img src={img2} alt='carousel-img' />
+          </div>
+          <div>
+            <img src={img3} alt='carousel-img' />
+          </div>
+        </Carousel>
+      </div>
     </div>
   )
 }
